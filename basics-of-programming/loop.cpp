28d10d05c98e6513f3b7b6dbb7076d3af07e7eb1
Akshata Jadahav <jakshata412@gmail.com>
2024-03-09 22:55:31 +0530

    conditionals and loops

---
+++ basics-of-programming/loop.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    int num = 10;
+
+    if(num > 0){
+        cout<< "Positive" << endl;
+    }
+    else if(num < 0){
+        cout<< "Negative" << endl;
+    }
+    else{
+        cout << "zero" << endl;
+    }
+    return 0;
+}
+
+// even or odd
+
+int main(){
+    
+int num = 7;
+
+if(num%2 ==0){
+    cout << "Even" << endl;
+}
+else{
+    cout << "Odd" << endl;
+    }
+}
