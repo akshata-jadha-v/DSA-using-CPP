22f1a81cc93528d7b9ae49a1af3c71890d30d76c
Akshata Jadahav <jakshata412@gmail.com>
2024-03-11 15:13:16 +0530

    hollow reactangle

--- basics-of-programming/loop.cpp
+++ basics-of-programming/loop.cpp
@@ -109,4 +109,30 @@ int main() {
     }
  }
 
+ // hollow rectangle
+
+ #include <iostream>
+ using namespace std;
+
+ int main() {
+
+  //outer loop
+ for(int row=0; row<5; row=row+1){
+    //inner loop
+    for(int col=0; col<5; col=col+1){
+        //if zeroth pr last row, then print star
+        if(row == 0 || row == 4){
+            cout << "* ";
+        }
+        else{
+            if(col == 0 || col == 4){
+                cout << "  ";
+            }
+        }
+    }
+    cout << endl;
+ }
+ }
+
+
 
