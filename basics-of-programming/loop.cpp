bb5b637a0691e9850a05526a07bf1b7d5df9b704
Akshata Jadahav <jakshata412@gmail.com>
2024-03-11 16:32:10 +0530

    half pyramid

--- basics-of-programming/loop.cpp
+++ basics-of-programming/loop.cpp
@@ -131,7 +131,22 @@ int main() {
         }
     }
     cout << endl;
+  }
  }
+
+ // half pyramid
+
+ #include <iostream>
+ using namespace std;
+
+ int main() {
+
+    for(int row=0; row<5; row=row+1) {
+        for(int col=0; col<row+1; col=col+1){
+            cout << "* ";
+        }
+        cout << endl;
+    }
  }
 
 
