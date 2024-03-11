a8ddf33ff9519ca6575279bca17bb9a2997749d0
Akshata Jadahav <jakshata412@gmail.com>
2024-03-11 16:51:13 +0530

    inverted numeric half pyramid

--- basics-of-programming/loop.cpp
+++ basics-of-programming/loop.cpp
@@ -185,6 +185,24 @@ int main() {
     }
  }
 
+ // inverted numeric half pyramid
+
+ #include <iostream>
+ using namespace std;
+
+ int man() {
+
+    int n;
+    cin >> n;
+
+    for(int row=0; row<n; row=row+1){
+        for(int col=0; col<n-row; col=col+1){
+            cout << col+1 << " ";
+        }
+    }
+    cout << endl;
+ }
+
 
 
 
