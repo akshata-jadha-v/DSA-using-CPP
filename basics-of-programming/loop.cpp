8fbc7b6b7bd863b6cb5d5fae88245e25c1d8724d
Akshata Jadahav <jakshata412@gmail.com>
2024-03-11 14:59:56 +0530

    rectangle patterns

--- basics-of-programming/loop.cpp
+++ basics-of-programming/loop.cpp
@@ -88,7 +88,25 @@ int main(){
         }
         cout << endl;
     }
-    }
+}
 
+// rectangle pattern
+
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    //outer loop -> rows
+    for( int i=0; i<3; i=i+1) {
+       //for each row, print 5 star
+       //inner loop -> print 5 stars
+
+       for(int j=0; j<5; j=j+1){
+        cout << " * ";
+       }
+       cout << endl;
+    }
+ }
 
 
