06cf5069de51ec2c8d4158b57010bac566662302
Akshata Jadahav <jakshata412@gmail.com>
2024-03-13 19:21:42 +0530

    basics

--- basics-of-programming/basic.cpp
+++
@@ -1,9 +0,0 @@
-// hello world program
-
-#include <iostream>
-using namespace std;
-
-int main() {
-
-    cout << "Hello World";
-}
---
+++ basics-of-programming/basics.cpp
@@ -0,0 +1,40 @@
+// hello world program
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+    cout << "Hello World";
+}
+
+// initalization and declaration
+
+#include <iostream>
+using namespace std;
+
+// int -> 4byte
+int main()
+{
+
+    int num = 50;
+    cout << num << endl;
+
+    cout << sizeof(num) << endl;
+
+    // character -> 1byte
+    char ch = 'k';
+    cout << ch << endl;
+    cout << sizeof(ch) << endl;
+
+    // float -> 4byte
+    float point = 1.69;
+    cout << point << endl;
+    cout << sizeof(point) << endl;
+
+    // long -> 4byte
+    long number = 23;
+    cout << number << endl;
+    cout << sizeof(number) << endl;
+}
