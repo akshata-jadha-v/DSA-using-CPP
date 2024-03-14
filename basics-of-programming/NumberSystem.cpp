82d1e824d91cdda313dd8657b853c69ae74fcfa3
Akshata Jadahav <jakshata412@gmail.com>
2024-03-14 20:22:30 +0530

    number system : binary and decimal

--- basics-of-programming/NumberSystem.cpp
+++ basics-of-programming/NumberSystem.cpp
@@ -1,8 +1,19 @@
-// Decimal to Binary Conversion
+// The decimalToBinary function takes an integer n as input and returns its binary representation as an integer.
+// Inside this function:
+// binaryNumber is initialized to 0 to store the binary representation.
+// i is used as a counter to keep track of the bit position.
+// A while loop extracts each bit of the decimal number n one by one:
+// int bit = n & 1; extracts the rightmost bit of n using bitwise AND.
+// The bit is then added to the binaryNumber at the appropriate position using the pow function and a right shift operation (n = n >> 1) is performed to get the next bit.
+// This process continues until n becomes 0.
 
 #include <iosream>
 #include <cmath>
 using namespace std;
+
+// Decimal to Binary Conversion
+
+// Method 1 by division
 
 int decimalToBinaryMethod1(int n)
 {
@@ -26,7 +37,7 @@ int main()
     cout << binary << endl;
 }
 
-// bitwise method
+// Method 2 by Bitwise
 
 int decimalToBinaryMethod2(int n)
 {
@@ -40,4 +51,26 @@ int decimalToBinaryMethod2(int n)
         n = n >> 1;
     }
     return binaryno;
+}
+
+// Binary to Decimal Conversion
+
+int binaryToDecimal(int n)
+{
+    int decimal = 0;
+    int i = 0;
+    while (n)
+    {
+        int bit = n % 10;
+        decimal = decimal + bit * pow(2, i++);
+        n /= 10;
+    }
+    return decimal;
+}
+
+int main()
+{
+    int binaryno;
+    cin >> binaryno;
+    cout << binaryToDecimal(binaryno) << endl;
 }
