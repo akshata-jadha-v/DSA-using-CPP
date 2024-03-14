b541d307b6e5c3ad9408e7b19be0e40f1036b458
Akshata Jadahav <jakshata412@gmail.com>
2024-03-14 13:36:31 +0530

    bitwise method

--- basics-of-programming/NumberSystem.cpp
+++ basics-of-programming/NumberSystem.cpp
@@ -1,3 +1,5 @@
+// Decimal to Binary Conversion
+
 #include <iosream>
 #include <cmath>
 using namespace std;
@@ -13,7 +15,7 @@ int decimalToBinaryMethod1(int n)
         binaryno = bit * pow(10, i++) + binaryno;
         n = n / 2;
     }
-    return0;
+    return binaryno;
 }
 int main()
 {
@@ -22,4 +24,20 @@ int main()
     cin >> n;
     int binary = decimalToBinaryMethod1(n);
     cout << binary << endl;
+}
+
+// bitwise method
+
+int decimalToBinaryMethod2(int n)
+{
+    // Bitwise Method
+    int binaryno = 0;
+    int i = 0;
+    while (n > 0)
+    {
+        int bit = (n & 1);
+        binaryno = bit * pow(10, i++) + binaryno;
+        n = n >> 1;
+    }
+    return binaryno;
 }
